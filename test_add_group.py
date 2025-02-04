4ead9436344982cb3de4ec00830203b59c824b6b
Askhat Karimov <gipopotamuss@gmail.com>
2025-02-04 16:59:06 +0300

    Создан тест на добавление группы и приведен к виду, который будет в дальнейших лекциях. Также перенесен файл geckodriver в system32

--- test_add_group.py
+++ test_add_group.py
@@ -12,12 +12,9 @@ class TestAddGroup(unittest.TestCase):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
 
-    
     def test_add_group(self):
         wd = self.wd
         wd.get("http://localhost/addressbook/")
-        wd.find_element_by_id("LoginForm").click()
-        wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys("admin")
         wd.find_element_by_name("pass").clear()
@@ -27,31 +24,30 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_name("new").click()
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("dfgdfgdfg")
+        wd.find_element_by_name("group_name").send_keys("hgjhgjghjg")
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("dfgdfhghfghf")
+        wd.find_element_by_name("group_header").send_keys("ghjghjghjghj")
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("fghfhfghfghf")
+        wd.find_element_by_name("group_footer").send_keys("ghjghjgjghj")
         wd.find_element_by_name("submit").click()
         wd.find_element_by_link_text("group page").click()
         wd.find_element_by_link_text("Logout").click()
-    
+
     def is_element_present(self, how, what):
         try: self.wd.find_element(by=how, value=what)
         except NoSuchElementException as e: return False
         return True
-    
+
     def is_alert_present(self):
         try: self.wd.switch_to_alert()
         except NoAlertPresentException as e: return False
         return True
-    
+
 
     def tearDown(self):
         self.wd.quit()
-
 
 if __name__ == "__main__":
     unittest.main()
