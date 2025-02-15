11386b5284b506c9294e2231143a759235aad843
Askhat Karimov <gipopotamuss@gmail.com>
2025-02-15 14:49:38 +0300

    Реализация создания элемента, если его нет для удаления и модификации групп; удаления и модификации контактов

--- fixture/contact.py
+++ fixture/contact.py
@@ -74,3 +74,8 @@ class ContactHelper:
     def return_to_homepage(self):
         wd = self.app.wd
         wd.find_element_by_link_text("home page").click()
+
+    def count(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("home").click()
+        return len(wd.find_elements_by_name("selected[]"))
--- test/test_bedit_contact.py
+++ test/test_bedit_contact.py
@@ -1,7 +1,11 @@
 from model.contact import Contact
 
 def test_edit_contact_firstname(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(firstname="test"))
     app.contact.edit_first_contact(Contact(firstname="New firstname"))
 
 def test_edit_contact_lastname(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(firstname="test"))
     app.contact.edit_first_contact(Contact(lastname="New lastname"))
