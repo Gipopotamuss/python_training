50cae305a107506081f5e54f664a4a488b03392c
Askhat Karimov <gipopotamuss@gmail.com>
2025-02-23 12:47:04 +0300

    Списки для групп (добавление, удаление, модификация)

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -5,10 +5,16 @@ from model.group import Group
 
 
 def test_add_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(name="hgjhgjghjg", header="ghjghjghjghj", footer="ghjghjgjghj"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
 
 def test_add_empty_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(name="", header="", footer=""))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
 
 def is_element_present(self, how, what):
     try: self.wd.find_element(by=how, value=what)
