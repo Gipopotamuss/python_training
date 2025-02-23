cbfaabf03218b4878a238505ec70083fd8b57b14
Askhat Karimov <gipopotamuss@gmail.com>
2025-02-23 15:06:00 +0300

    Сортировка списков перед сравнением в группах (добавление и модификация)

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -6,15 +6,22 @@ from model.group import Group
 
 def test_add_group(app):
     old_groups = app.group.get_group_list()
-    app.group.create(Group(name="hgjhgjghjg", header="ghjghjghjghj", footer="ghjghjgjghj"))
+    group = Group(name="hgjhgjghjg", header="ghjghjghjghj", footer="ghjghjgjghj")
+    app.group.create(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+
 
 def test_add_empty_group(app):
     old_groups = app.group.get_group_list()
-    app.group.create(Group(name="", header="", footer=""))
+    group = Group(name="", header="", footer="")
+    app.group.create(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 def is_element_present(self, how, what):
     try: self.wd.find_element(by=how, value=what)
