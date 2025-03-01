e2541f947266bacb6c681fd9f43174a07ddeb63c
Askhat Karimov <gipopotamuss@gmail.com>
2025-03-01 21:24:12 +0300

    Параметризация тест для добавления контакта

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,14 +1,40 @@
 # -*- coding: utf-8 -*-
+import pytest
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 from model.contact import Contact
+import random
+import string
 
 
-def test_add_contact(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits +  " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+testdata = [Contact(firstname="", middlename="", lastname="", nickname="",
+                    title="", company="", address="", home="", mobile="",
+                    work="", fax="", email_1="", email_2="", email_3="",homepage="")] + [
+    Contact(firstname=random_string("firstname", 10),
+            middlename=random_string("middlename", 10),
+            lastname=random_string("lastname", 10),
+            nickname=random_string("nickname", 10),
+            title=random_string("title", 10),
+            company=random_string("company", 10),
+            address=random_string("address", 10),
+            home=random_string("home", 10),
+            mobile=random_string("mobile", 10),
+            work=random_string("work", 10),
+            fax=random_string("fax", 10),
+            email_1=random_string("email_1", 10),
+            email_2=random_string("email_2", 10),
+            email_3=random_string("email_3", 10),
+            homepage=random_string("homepage", 10))
+    for i in range(2)
+]
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
-    contact = Contact(firstname="dfgdfgdf", middlename="dfgdfg", lastname="dfgdfg", nickname="dfgdfgdfg", title="dfgdfgdfg", company="dfgdgdfg", address="dfgdfgdfgdfg",
-                               home="dfgdgdf", mobile="dfgdgdfg", work="dfgdgdfg", fax="dfgdfgdfg", email_1="dfgdgdfg", email_2="dfgdfgdfg", email_3="dfgdgdfgd",
-                               homepage="dfgdfgdfgdfg")
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -10,7 +10,7 @@ def random_string(prefix, maxlen):
     symbols = string.ascii_letters + string.digits +  " "*10
     return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
-testdata = [
+testdata = [Group(name="", header="", footer="")] + [
     Group(name=random_string("name", 10),
           header=random_string("header", 20),
           footer=random_string("footer", 20))
@@ -19,7 +19,6 @@ testdata = [
 
 @pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
 def test_add_group(app, group):
-    pass
     old_groups = app.group.get_group_list()
     app.group.create(group)
     assert len(old_groups) + 1 == app.group.count()
