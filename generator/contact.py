99ffad1b53b0a136321868385ff08e0b528073c1
Askhat Karimov <gipopotamuss@gmail.com>
2025-03-08 14:55:22 +0300

    Генерация тестовых данных в формате JSON для добавления контактов и групп

---
+++ generator/contact.py
@@ -0,0 +1,52 @@
+from model.contact import Contact
+import random
+import string
+import os.path
+import json
+import getopt
+import sys
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "n:f:", ["number of contacts", "file"])
+except getopt.GetoptError as err:
+    getopt.usage()
+    sys.exit(2)
+
+n = 2
+f = "Data/contacts.json"
+
+for o, a in opts:
+    if o == "-n":
+        n = int(a)
+    elif o == "-f":
+        f = a
+
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits +  " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+testdata = [Contact(firstname="", middlename="", lastname="", nickname="",
+                    title="", company="", address="", home="", mobile="",
+                    work="", fax="", email_1="", email_2="", email_3="",homepage="")] + [
+    Contact(firstname=random_string("firstname", 10),
+            middlename=random_string("middlename", 10),
+            lastname=random_string("lastname", 10),
+            nickname=random_string("nickname", 10),
+            title=random_string("title", 10),
+            company=random_string("company", 10),
+            address=random_string("address", 10),
+            home=random_string("home", 10),
+            mobile=random_string("mobile", 10),
+            work=random_string("work", 10),
+            fax=random_string("fax", 10),
+            email_1=random_string("email_1", 10),
+            email_2=random_string("email_2", 10),
+            email_3=random_string("email_3", 10),
+            homepage=random_string("homepage", 10))
+    for i in range(n)
+]
+
+file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
+
+with open(file, "w") as out:
+    out.write(json.dumps(testdata, default=lambda x: x.__dict__, indent=2))
