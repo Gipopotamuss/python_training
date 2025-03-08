af370f3ce01a59b9d308154b2924a0aeb2e33bc6
Askhat Karimov <gipopotamuss@gmail.com>
2025-03-08 15:57:32 +0300

    параметризация тестов: динамическая генерация тестов для добавления контактов

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
-import pytest
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 from model.contact import Contact
-from data.contacts import constant as testdata
 
-@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
-def test_add_contact(app, contact):
+
+def test_add_contact(app, data_contacts):
+    contact = data_contacts
     old_contacts = app.contact.get_contact_list()
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
