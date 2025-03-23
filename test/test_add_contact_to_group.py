a54315ad2abaef6580a2b66f8697d4cb3e5c20ca
Askhat Karimov <gipopotamuss@gmail.com>
2025-03-23 12:04:13 +0300

    Добавление контакта в группу, где этого контакта нет

--- test/test_add_contact_to_group.py
+++ test/test_add_contact_to_group.py
@@ -4,14 +4,14 @@ from model.group import Group
 
 
 def test_add_user_to_group(app, orm, db):
-    if len(db.get_contact_list()) == 0:
-        app.user.create(Contact(firstname="firstname", lastname="lastname"))
     if len(db.get_group_list()) == 0:
         app.group.create(Group(name="test"))
-    all_contacts = orm.get_contact_list()
-    contact = random.choice(all_contacts)
     groups = orm.get_group_list()
     group_id = random.choice(groups).id
+    if len(orm.get_contacts_not_in_group(Group(id=group_id))) == 0:
+        app.contact.create(Contact(firstname="firstname", lastname="lastname"))
+    contacts = orm.get_contacts_not_in_group(Group(id=group_id))
+    contact = random.choice(contacts)
     app.contact.add_contact_to_group(contact.id, group_id)
-    new_users = orm.get_contacts_in_group(Group(id=group_id))
-    assert contact in new_users
+    new_contacts = orm.get_contacts_in_group(Group(id=group_id))
+    assert contact in new_contacts
